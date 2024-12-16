dd95cf2876f2782581dd51f82d077d0206ba449b
Martin Sanchez <tinchofiuba@gmail.com>
2024-12-16 19:14:41 -0300

    hay un error cuando cargo 2 veces el json de calib.

--- ProgramaSalpicaduras/modelSalpicaduras.py
+++ ProgramaSalpicaduras/modelSalpicaduras.py
@@ -17,10 +17,6 @@ class Model:
         self.ensayosAnteriores=None
         self.resultados=None
         #imprimo la ubicacion de ejcucion
-        with open('json/ensayosAnteriores.json','r') as file:
-            self.ensayosAnteriores=json.load(file)
-        with open('json/calibracion.json','r') as file:
-            self.calibracion=json.load(file)
             
     def resetTiempoPasos(self):
         print("hola")
--- ProgramaSalpicaduras/viewSalpicaduras.py
+++ ProgramaSalpicaduras/viewSalpicaduras.py
@@ -18,47 +18,71 @@ class GUI_Salpicaduras(QDialog, Ui_Dialog):
         self.configLimitesliders("ambos")
         #prohibo que la aplicación se redimensione
         self.setFixedSize(self.size())
+        self.comboBoxVelocidadesCalib.setEnabled(False)
+        self.comboBoxVelocidadesCalib.currentIndexChanged.connect(self.actualizarValoresPorcomboBox)
         self.horizontalSliderDelayPasos.valueChanged.connect(self.configSliderDelayPasos)
         self.horizontalSliderTiempoEnsayo.valueChanged.connect(self.configSliderTiempoEnsayo)
         self.pushButtonResetDelayPasos.clicked.connect(lambda: self.configLimitesliders("delayPasos"))
         self.pushButtonResetTiempoEnsayo.clicked.connect(lambda: self.configLimitesliders("tiempoEnsayo"))
         self.pushButtonCargarConfigCalib.clicked.connect(self.cargarConfiguracion)
-        
+        self.pushButtonGuardarConfigCalib.clicked.connect(self.guardarConfiguracion)
+    
     def cargarConfiguracion(self):
-        #abro una ventana de dialogo que me permita seleccionar un archivo json para luego abrirlo
-        #y cargar los valores en los widgets correspondientes
+        file = QFileDialog.getOpenFileName(self, "Abrir archivo de calibración", "", "JSON Files (*.json)")[0]
+        print(file)
+        try :
+            with open(file,'r') as file:
+                print("archivo seleccionado")
+                self.calibracion=json.load(file)
+                self.comboBoxVelocidadesCalib.setEnabled(True)
+                self.pushButtonGuardarConfigCalib.setEnabled(True)
+                self.comboBoxVelocidadesCalib.clear()
+                self.comboBoxVelocidadesCalib.addItems(self.calibracion.keys())
+                self.horizontalSliderDelayPasos.setValue(self.calibracion[self.comboBoxVelocidadesCalib.currentText()]["delayPasos"]) 
+                self.horizontalSliderTiempoEnsayo.setValue(self.calibracion[self.comboBoxVelocidadesCalib.currentText()]["tiempoEnsayo"])
+                print("archivo cargado")
+        except:
+            print("error al cargar el archivo Json, o error al leer los valores")
+            
+    def actualizarValoresPorcomboBox(self):
+                self.horizontalSliderDelayPasos.setValue(self.calibracion[self.comboBoxVelocidadesCalib.currentText()]["delayPasos"]) 
+                self.horizontalSliderTiempoEnsayo.setValue(self.calibracion[self.comboBoxVelocidadesCalib.currentText()]["tiempoEnsayo"])
         
-        #abro la ventana de dialogo
-        file = QFileDialog.getOpenFileName(self, "Abrir archivo de calibración", "", "JSON Files (*.json)")
-        
-        
+                
     def configLimitesliders(self,slider): #en función del argumento reseteo, o configuro uno/los dos sliders
+        self.delayPasosMin=400
+        self.delayPasosMax=2000
+        self.tiempoEnsayoMin=400
+        self.tiempoEnsayoMax=1000
         if slider=="ambos":
-            self.horizontalSliderDelayPasos.setMinimum(400) #valor del tiempo en microSegundos
-            self.horizontalSliderDelayPasos.setMaximum(2000) #valor del tiempo en microSegundos
-            self.horizontalSliderTiempoEnsayo.setMinimum(400) #valor del tiempo en milisegundos
-            self.horizontalSliderTiempoEnsayo.setMaximum(1000) #valor del tiempo en milisegundos
+            self.horizontalSliderDelayPasos.setMinimum(self.delayPasosMin) #valor del tiempo en microSegundos
+            self.horizontalSliderDelayPasos.setMaximum(self.delayPasosMax) #valor del tiempo en microSegundos
+            self.horizontalSliderTiempoEnsayo.setMinimum(self.tiempoEnsayoMin) #valor del tiempo en milisegundos
+            self.horizontalSliderTiempoEnsayo.setMaximum(self.tiempoEnsayoMax) #valor del tiempo en milisegundos
         elif slider=="delayPasos":
-            self.horizontalSliderDelayPasos.setMinimum(400) #valor del tiempo en microSegundos
-            self.horizontalSliderDelayPasos.setMaximum(2000) #valor del tiempo en microSegundos
+            self.horizontalSliderDelayPasos.setMinimum(self.delayPasosMin) #valor del tiempo en microSegundos
+            self.horizontalSliderDelayPasos.setMaximum(self.delayPasosMax) #valor del tiempo en microSegundos
         else :
-            self.horizontalSliderTiempoEnsayo.setMinimum(400) #valor del tiempo en milisegundos
-            self.horizontalSliderTiempoEnsayo.setMaximum(1000) #valor del tiempo en milisegundos
+            self.horizontalSliderTiempoEnsayo.setMinimum(self.tiempoEnsayoMin) #valor del tiempo en milisegundos
+            self.horizontalSliderTiempoEnsayo.setMaximum(self.tiempoEnsayoMax) #valor del tiempo en milisegundos
     
     def configSliderDelayPasos(self):
         self.labelValorDelayPasos.setText(str(self.horizontalSliderDelayPasos.value()))
-        self.guardarConfiguracion()
+        self.verificacionGuardarConfiguracion()
         
     def configSliderTiempoEnsayo(self):
         self.labelValorTiempoEnsayo.setText(str(self.horizontalSliderTiempoEnsayo.value()))
-        self.guardarConfiguracion()
+        self.verificacionGuardarConfiguracion()
         
-    def guardarConfiguracion(self):
+    def verificacionGuardarConfiguracion(self):
         #verifico que tanto el otro slider como el lineedit esten OK
-        if self.horizontalSliderTiempoEnsayo.value()!=0 and self.horizontalSliderDelayPasos.value()!=0:
+        if self.horizontalSliderTiempoEnsayo.value()>=self.tiempoEnsayoMin and self.horizontalSliderDelayPasos.value()>=self.delayPasosMin and self.comboBoxVelocidadesCalib.isEnabled():
             self.pushButtonGuardarConfigCalib.setEnabled(True)
         else:
             self.pushButtonGuardarConfigCalib.setEnabled(False)
+            
+    def guardarConfiguracion(self):
+        print("hola")
 
 
 if __name__ == '__main__':
